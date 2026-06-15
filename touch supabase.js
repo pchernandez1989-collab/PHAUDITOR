1cebd6224ad50ee8e8d28558db1d7c0cf522e293
pchernandez1989-collab <pchernandez1989@gmail.com>
2026-06-15 15:45:30 -0500

    Update touch supabase.js

--- touch supabase.js
+++ touch supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = "https://xybxmetzpngkoqnnfqip.supabase.co"; 
+const supabaseKey = "sb_publishable__Ew3BfG2XAn2WiOUBGMijw_mUKST1J3"; 
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
